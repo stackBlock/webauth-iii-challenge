25480d6dfe9dfac7b2107bcf37c225e2f13b2e42
anthony <stackchain@gmail.com>
2019-06-12 17:44:22 -0400

    commit - token working

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const Users = require('../../users/user-model.js');
+const secrets = require('../../config/secrets.js');
 
 router.post('/', (req, res) => {
     let { username, password } = req.body;
@@ -10,8 +11,13 @@ router.post('/', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
-                // req.session.user = user;
-                res.status(200).json({ message: `Welcome ${user.username}!` });
+                const token = generateToken(user);
+                res
+                    .status(200)
+                    .json({
+                        message: `Welcome ${user.username}!`,
+                        token,
+                    });
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' });
             }
@@ -20,5 +26,17 @@ router.post('/', (req, res) => {
             res.status(500).json(error);
         });
 });
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id, //standard claim
+        username: user.username,
+        roles: ['student']
+    };
+    const options = {
+        expiresIn: '8h',
+    };
+    return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
